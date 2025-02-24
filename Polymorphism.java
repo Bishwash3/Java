2d1c8e8bd38f11a2255dca478657b14b7fa3abcb
Bishwash3 <bhusalbiswash2.com>
2025-02-24 08:46:28 +0545

    polymorphism

--- Polymorphism.java
+++ Polymorphism.java
@@ -23,5 +23,24 @@ public class Polymorphism {
     double add(double a, double b){
         return a+b;
     }
+    public static void main(String[] args){
+        Polymorphism addition = new Polymorphism();
+        int firstReasult = addition.add(10, 20);
+        System.out.println(firstReasult);
+
+        System.out.println("--------------------");
+
+        Polymorphism addition2 = new Polymorphism();
+        int secondReasult = addition2.add(10,20,30);
+        System.out.println(secondReasult);
+
+        System.out.println("--------------------");
+
+        Polymorphism addition3 = new Polymorphism();
+        double thirdReasult = addition3.add(10,20);
+        System.out.println(thirdReasult);
+
+
+    }
     
 }
